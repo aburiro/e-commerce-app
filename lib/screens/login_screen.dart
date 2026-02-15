0fea878fdd3dbef88e3abfcc75dd991b7098621a
aburiro <96950982+aburiro@users.noreply.github.com>
2026-02-15 13:10:55 +0500

    login-screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'YourApp Login',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
-      home: const LoginScreenAnimated(),
-    );
-  }
-}
-
 class LoginScreenAnimated extends StatefulWidget {
   const LoginScreenAnimated({Key? key}) : super(key: key);
 
