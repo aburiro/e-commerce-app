3efa5448eae291d331c43289803765a6784f9ae7
aburiro <96950982+aburiro@users.noreply.github.com>
2026-04-08 00:26:19 +0500

    main-dart-file

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ecommerce App',
+      title: 'E-commerce App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF6C63FF)),
